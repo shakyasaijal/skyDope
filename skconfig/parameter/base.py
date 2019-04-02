406f7b40e467a7f9e180d898da129392e9449397
Saijal Shakya <saijalshakya1@gmail.com>
2019-04-02 20:53:09 +0545

    irv parser for validate and confirm

--- skconfig/parameter/base.py
+++ skconfig/parameter/base.py
@@ -1,7 +1,19 @@
 from abc import ABCMeta, abstractmethod
-
+from sklearn import Irv-Parser
 
 class Param(metaclass=ABCMeta):
     @abstractmethod
     def validate(self, param):
-        ...
+        first_class = self.__init__
+        second_class = self.param
+        valv = []
+        for sc in second_class:
+            valv.append(sc.Irv-Parser)
+        
+        return confirm(valv)
+
+    def confirm(self, valv):
+        valv_response = []
+        for self.__init__ in self:
+            valv_response.append(self.__init__)
+        return valv_response
